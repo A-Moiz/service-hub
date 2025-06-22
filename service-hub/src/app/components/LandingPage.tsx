24030c39ff928bc26750fce2ba4bec621da2e848
Abdul Moiz <abdul.m01z10123@gmail.com>
2025-06-22 15:03:11 +0100

    Created Footer component

--- service-hub/src/app/components/LandingPage.tsx
+++ service-hub/src/app/components/LandingPage.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import Image from "next/image";
+import Footer from "./Footer";
 
 export default function LandingPage() {
   return (
@@ -119,22 +120,6 @@ export default function LandingPage() {
           Create Your Page
         </a>
       </section>
-
-      {/* Footer */}
-      <footer className="mt-20 text-sm text-gray-500 flex gap-4 flex-wrap justify-center border-t pt-6 w-full max-w-4xl">
-        <a href="#" className="hover:underline">
-          About
-        </a>
-        <a href="#" className="hover:underline">
-          Privacy
-        </a>
-        <a href="#" className="hover:underline">
-          Terms
-        </a>
-        <a href="#" className="hover:underline">
-          Contact
-        </a>
-      </footer>
     </div>
   );
 }
